dde2291cb0bf82d30b7f9410fd0d49e7650b5af4
GC9810 <111528244+GC9810@users.noreply.github.com>
2023-03-10 22:33:01 -0700

    Update shortcut.js

--- shortcut.js
+++ shortcut.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RMS Shortcuts
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      1.2
 // @description  Keyboard shortcuts for RMS Asset Detail page
 // @author       Your name
 // @match        https://rms.pason.com/Pages/Components/ComponentDetails.aspx?id=*
@@ -16,12 +16,13 @@
         let createBtn = document.getElementById("ctl00_PageHeaderNavigationContent_CreateSimilarButton");
         let barcodeInput = document.getElementById("ctl00_BarcodeNumericTextBox");
 
+        //Shortcut listener will only work when the focus is not on any input element.
         document.addEventListener('keydown', function(event) {
-            if (event.key.toLowerCase() === "e") {
+            if (event.target.tagName.toLowerCase() !== 'input' && event.key.toLowerCase() === "e") {
                 updateBtn.click();
-            } else if (event.key.toLowerCase() === "c") {
+            } else if (event.target.tagName.toLowerCase() !== 'input' && event.key.toLowerCase() === "c") {
                 createBtn.click();
-            } else if (event.key.toLowerCase() === "d") {
+            } else if (event.target.tagName.toLowerCase() !== 'input' && event.key.toLowerCase() === "d") {
                 barcodeInput.focus();
                 event.preventDefault();
             }
