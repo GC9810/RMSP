a7fcfb06d7d278df5f5885568afeba32e6b1e97d
GC9810 <111528244+GC9810@users.noreply.github.com>
2023-03-10 17:36:10 -0700

    Create DIS138.js

---
+++ DIS138.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         RMS Mod (DIS138)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Autofocus and Highlight Input Box in RMS
+// @author       You
+// @match        https://rms.pason.com/Pages/Components/ComponentEdit.aspx?id=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener('load', function() {
+        let input = document.getElementById("ctl00_ProgessIndicationPageBodyContent_UpdatedComponentProperties_PropertyRepeater_ctl01_TextProperty");
+        input.select();
+        input.focus();
+
+        input.addEventListener("keydown", function(event) {
+            if (event.key.toLowerCase() === "enter") {
+                event.preventDefault();
+                let link = document.querySelector("a[href*='ctl00$ProgessIndicationPageBodyContent$ctl13']");
+                link.click();
+            }
+        });
+    });
+})();
