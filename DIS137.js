a05afdc6617831616b4249b9360355d0e753eb84
GC9810 <111528244+GC9810@users.noreply.github.com>
2023-03-12 23:16:17 -0600

    Create DIS137.js

---
+++ DIS137.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         RMS Mod (DIS137)
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  Autofocus and Highlight Input Box in RMS
+// @author       You
+// @match        https://rms.pason.com/Pages/Components/ComponentEdit.aspx?id=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener('load', function() {
+        let input = document.getElementById("ctl00_ProgessIndicationPageBodyContent_UpdatedComponentProperties_PropertyRepeater_ctl01_TextProperty");
+        if (input) {
+            input.select();
+            input.focus();
+
+            input.addEventListener("keydown", function(event) {
+                if (event.key.toLowerCase() === "enter") {
+                    event.preventDefault();
+                    let link = document.querySelector("a[href*='ctl00$ProgessIndicationPageBodyContent$ctl13']");
+                    if (link) {
+                        link.click();
+                    }
+                }
+            });
+        }
+
+        document.addEventListener('keydown', function(event) {
+            if (event.key.toLowerCase() === 'c') {
+                let changebtn = document.getElementById('ctl00_ProgessIndicationPageBodyContent_ConfirmChangesButton');
+                if (changebtn) {
+                    changebtn.focus();
+                }
+            }
+        });
+
+        const table = document.getElementById("ctl00_ProgessIndicationPageBodyContent_UpdatedComponentFlags_ComponentFlagDataList");
+        if (table) {
+            const checkboxes = table.querySelectorAll("input[type=checkbox]");
+            //uncheck all the flags
+            checkboxes.forEach(checkbox => {
+                checkbox.checked = false;
+            });
+        }
+
+    });
+})();
