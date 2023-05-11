9f133f2a35cc6bc126a9d30025ace3476984109b
GC9810 <111528244+GC9810@users.noreply.github.com>
2023-05-11 17:37:22 -0600

    Update VSP015.js

--- VSP015.js
+++ VSP015.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RMS Mod (For VSP015)(enter version)
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Mod for RMS entry (enter version)
 // @author       You
 // @match        https://rms.pason.com/Pages/Components/ComponentAdd.aspx?componentTemplate=*
@@ -12,46 +12,53 @@
 (function() {
     'use strict';
 
-        let mac = document.getElementById("ctl00_ProgessIndicationPageBodyContent_Properties_PropertyRepeater_ctl00_TextProperty")
-        let rev = document.getElementById("ctl00_ProgessIndicationPageBodyContent_Properties_PropertyRepeater_ctl01_TextProperty")
-        let sn = document.getElementById("ctl00_ProgessIndicationPageBodyContent_Properties_PropertyRepeater_ctl02_TextProperty")
-        let origin = document.getElementById("ctl00_ProgessIndicationPageBodyContent_OriginCountryDropDownList")
-        let country = document.getElementById("ctl00_ProgessIndicationPageBodyContent_CountryDropDownList")
-        let barcode = document.getElementById("ctl00_ProgessIndicationPageBodyContent_BarcodeTextBox")
-        let add_btn = document.getElementById("ctl00_ProgessIndicationPageBodyContent_TopAddItemButton")
+    const mac = document.getElementById("ctl00_ProgessIndicationPageBodyContent_Properties_PropertyRepeater_ctl00_TextProperty");
+    const rev = document.getElementById("ctl00_ProgessIndicationPageBodyContent_Properties_PropertyRepeater_ctl01_TextProperty");
+    const sn = document.getElementById("ctl00_ProgessIndicationPageBodyContent_Properties_PropertyRepeater_ctl02_TextProperty");
+    const origin = document.getElementById("ctl00_ProgessIndicationPageBodyContent_OriginCountryDropDownList");
+    const country = document.getElementById("ctl00_ProgessIndicationPageBodyContent_CountryDropDownList");
+    const barcode = document.getElementById("ctl00_ProgessIndicationPageBodyContent_BarcodeTextBox");
+    const add_btn = document.getElementById("ctl00_ProgessIndicationPageBodyContent_TopAddItemButton");
 
-        document.getElementById("ctl00_ProgessIndicationPageBodyContent_Revisions_RevisionDataList_ctl03_RevisionCheckBox").checked = true
+    document.getElementById("ctl00_ProgessIndicationPageBodyContent_Revisions_RevisionDataList_ctl03_RevisionCheckBox").checked = true;
 
-        country.value = 18 //select USA
-        origin.value = 48 //china
-        rev.value = "04" //fillup 04 in rev field
+    country.value = 18; //select USA
+    origin.value = 48; //china
+    rev.value = "04"; //fillup 04 in rev field
 
-        //change tabIndex sequence
-        barcode.tabIndex = 1
-        mac.tabIndex = 2
-        sn.tabIndex = 3
-        add_btn.tabIndex = 4
+    //change tabIndex sequence
+    barcode.tabIndex = 1;
+    mac.tabIndex = 2;
+    sn.tabIndex = 3;
+    add_btn.tabIndex = 4;
 
-        barcode.addEventListener('keydown', function(e){
-            if (e.keyCode === 13) { //treat enter key as tab key
-                e.preventDefault();
-                mac.focus();
-            }
-        });
+    barcode.addEventListener('keydown', function(e) {
+        if (e.keyCode === 13) { //treat enter key as tab key
+            e.preventDefault();
+            mac.focus();
+        }
+    });
 
-        mac.addEventListener('keydown', function(e){
-            if (e.keyCode === 13) { //treat enter key as tab key
-                e.preventDefault();
-                sn.focus();
-            }
-        });
+    mac.addEventListener('keydown', function(e) {
+        if (e.keyCode === 13) { //treat enter key as tab key
+            e.preventDefault();
+            sn.focus();
+        }
+    });
 
-        sn.addEventListener('keydown', function(e){
-            if (e.keyCode === 13) { //treat enter key as tab key
-                e.preventDefault();
-                add_btn.focus();
-            }
-        });
+    sn.addEventListener('keydown', function(e) {
+        if (e.keyCode === 13) { //treat enter key as tab key
+            e.preventDefault();
+            add_btn.focus();
+        }
+    });
 
-        barcode.focus();
+    sn.addEventListener('blur', function() {
+        if (sn.value.toLowerCase().startsWith('s')) { //check for leading 's'
+            sn.value = sn.value.slice(1); //remove leading 's'
+            navigator.clipboard.writeText(sn.value); //copy new value to clipboard
+        }
+    });
+
+    barcode.focus();
 })();
